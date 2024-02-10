073b1d43b8257cff9abb1939205194702ad20e56
vietmrvu <viet.mrvu@gmail.com>
2024-02-10 09:16:38 +0700

    done

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Loader from "./Loader";
+import HomeInfo from "./HomeInfo";
+import Navbar from "./Navbar";
+import Alert from "./Alert";
+import CTA from "./CTA"
+import Footer from "./Footer"
+
+export {
+    Alert,
+    Navbar,
+    Loader,
+    HomeInfo,
+    CTA,
+    Footer
+}
---
+++ src/pages/About.jsx
@@ -0,0 +1,125 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { Link } from "react-router-dom";
+
+import { CTA } from "../components";
+import { experiences, skills } from "../constants";
+
+import "react-vertical-timeline-component/style.min.css";
+
+const About = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Mình là {" "}
+        <span className='blue-gradient_text font-semibold drop-shadow'>
+          {" "}
+          Vũ Trí Việt
+        </span>{" "}
+        👋
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+        Hiện là học sinh THCS Nguyễn Du Q1, đã tham gia những cuộc thi như NCKH, Tin học trẻ, Digitrans 
+        Edtech,...
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>Những kỹ năng của mình</h3>
+
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20' key={skill.name}>
+              <div className='btn-back rounded-xl' />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div id="projects" className='py-16'>
+        <h3 className='subhead-text'>Kinh nghiệm qua các cuộc thi</h3>
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>
+          Mình đã tham gia nhiều cuộc thi, nâng cao kỹ năng của mình và
+            hợp tác với những bạn thông minh. Dưới đây là một vài cuộc thi mình tham gia:
+          </p>
+        </div>
+
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                iconStyle={{ background: experience.iconBg }}
+                icon={
+                  <div className='flex justify-center items-center w-full h-full'>
+                    <img
+                      src={experience.icon}
+                      alt={experience.company_name}
+                      className='w-[60%] h-[60%] object-contain'
+                    />
+                  </div>
+                }
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: "none",
+                }}
+              >
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {experience.title}
+                  </h3>
+                  <p
+                    className='text-black-500 font-medium text-base'
+                    style={{ margin: 0 }}
+                  >
+                    {experience.company_name}
+                  </p>
+                </div>
+
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className='text-black-500/50 font-normal pl-1 text-sm'
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+                <Link
+                  to={experience.linkicon}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='font-semibold text-blue-600'
+                >
+                  Link dự án
+                </Link>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+
+      <hr className='border-slate-200' />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default About;
