1ebadd6b90dae89805b92833429e780f0e17549f
vietmrvu <viet.mrvu@gmail.com>
2024-02-19 11:21:45 +0700

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import Home from "./Home";
 import About from './About'
 import Projects from "./Projects";
 import Contact from "./Contact";
+import Game from "./Game";
 
 export {
     Home,
     About,
     Projects,
-    Contact
+    Contact,
+    Game
 }
